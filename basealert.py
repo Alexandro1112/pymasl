135a7b17b92d654b699483d0121e65eee14f1d65
Alexandro1112 <112175002+Alexandro1112@users.noreply.github.com>
2025-01-24 11:41:58 +0300

    Update basealert.py
    add list_available_fonts

--- basealert.py
+++ basealert.py
@@ -1,6 +1,7 @@
 from .constants import *
 import objc
 import AppKit
+import CoreText
 
 
 class Alert:
@@ -8,16 +9,18 @@ class Alert:
     def __str__(self):
         return f'<{Alert.__name__} object at 0x10cc92910>'
 
+    def list_available_fonts(self):
+        """Return all available fonts on current machine"""
+        font_descriptors = CoreText.CTFontManagerCopyAvailableFontFamilyNames()
+        list_fonts = []
+        for font in font_descriptors:
+            list_fonts.append(font)
+        return list_fonts
+
     @objc._objc.python_method
-    def create_alert(self, title, message, width, height, icon: [INFO_ICON, WARNING_ICON, TRASH_ICON, SYS_ICON,
-                                                                 STOP_ICON, SECURITY_ICON, ADVANCE_ICON, CAMERA_ICON,
-                                                                 COLOR_ICON, CRAYONS_ICON, DOCUMENT_ICON, FOLDER_ICON,
-                                                                 HOME_ICON, NETWORK_ICON, TOOLBAR_ICON, USER_ICON,
-                                                                 COMPUTER_ICON, CHECK_MARK_ICON],
-                                                                 buttons: [tuple, list, str]):
+    def create_alert(self, title, message, width, height, icon, buttons: [tuple, list, str]):
         """Create Alert instance."""
         self.buttons = buttons
-
         self.alert = AppKit.NSAlert.alloc().init()
         self.alert.setShowsSuppressionButton_(icon)
         icn = AppKit.NSImage.alloc().initWithContentsOfFile_(icon)
@@ -30,72 +33,51 @@ class Alert:
         if isinstance(buttons, str):
             i = buttons
             self.alert.addButtonWithTitle_(i)
-
         else:
             for i in buttons:
                 self.alert.addButtonWithTitle_(i)
                 for j in range(len(buttons)):
                     self.dict_btn = buttons
+
         try:
-            styles = {
-                'warning': AppKit.NSImage.imageNamed_('NSCaution'),
-                'check_mark': AppKit.NSImage.imageNamed_('NSMenuOnStateTemplate'),
-                'security': AppKit.NSImage.imageNamed_('NSSecurity'),
-                'document': AppKit.NSImage.imageNamed_('NSMultipleDocuments'),
-                'user': AppKit.NSImage.imageNamed_('NSUser'),
-                'crayons': AppKit.NSImage.imageNamed_('NSColorPickerCrayon'),
-                'toolbar': AppKit.NSImage.imageNamed_('NSToolbarCustomizeToolbarItemImage'),
-                'info': AppKit.NSImage.imageNamed_('NSInfo'),
-                'camera': AppKit.NSImage.imageNamed_('NSTouchBarCommunicationVideoTemplate'),
-                'home': AppKit.NSImage.imageNamed_('NSHomeTemplate'),
-                'trash': AppKit.NSImage.imageNamed_('NSTrashFull'),
-                'network': AppKit.NSImage.imageNamed_('NSNetwork'),
-                'color': AppKit.NSImage.imageNamed_('NSColumnViewTemplate'),
-                'stop': AppKit.NSImage.imageNamed_('NSStopProgressTemplate'),
-                'folder': AppKit.NSImage.imageNamed_('NSFolder'),
-                'sys': AppKit.NSImage.imageNamed_('NSPreferencesGeneral'),
-                'computer':  AppKit.NSImage.imageNamed_('NSComputer'),
-                'advance': AppKit.NSImage.imageNamed_('NSAdvanced'),
-                'glass': AppKit.NSImage.imageNamed_('NSSmallMagnifyingGlass'),
-            }
             if icon in styles.keys():
                 self.alert.setIcon_(styles[icon])
             else:
                 self.alert.setIcon_(icn)
         except KeyError:  # If ``icon`` custom image.
             self.alert.setIcon_(icn)
-        finally:
-            return self
 
-    def add_entry(self, entry_width, entry_height,
-                  entry_border, entry_font, entry_font_size,
-                  entry_color, entry_text_color):
+    def add_entry(self, width, height,
+                  border, font, font_size,
+                  color, text_color):
         """Add entry widget to alert."""
 
-        self.text_field = AppKit.NSTextField.alloc().initWithFrame_(((0, 0), (entry_width, entry_height)))
-        if entry_border is not None:
-            self.text_field.setCornerRadius_(entry_border)
-        if entry_text_color is not None:
-            text_color = eval('AppKit.NSColor.%sColor()' % entry_text_color)
+        self.text_field = AppKit.NSTextField.alloc().initWithFrame_(((0, 0), (width, height)))
+        if border is not None:
+            self.text_field.setCornerRadius_(border)
+        if text_color is not None:
+            text_color = eval('AppKit.NSColor.%sColor()' % text_color.lower())
             self.text_field.setTextColor_(text_color)
-        if entry_font is not None and entry_font_size is not None:
-            self.text_field.setFont_(AppKit.NSFont.fontWithName_size_(entry_font, entry_font_size))
-        if entry_color is not None:
-            color = eval('AppKit.NSColor.%sColor()' % entry_color)  # Strange name function in
+        if font is not None and font_size is not None:
+            if font in self.list_available_fonts():
+                self.text_field.setFont_(AppKit.NSFont.fontWithName_size_(font, font_size))
+            raise NameError('Font not supported on device or not found')
+        if color is not None:
+            color = eval('AppKit.NSColor.%sColor()' % color.lower())  # Strange name function in
             self.text_field.setBackgroundColor_(color)
         self.alert.setAccessoryView_(self.text_field)  # display widget on alert.
 
-    def add_slider(self, slider_color, slider_width,
-                   slider_height,
-                   slider_tracker_color,
+    def add_slider(self, color, width,
+                   height,
+                   tracker_color,
                    slider_default_value=0,
                    slider_max_value=None, slider_min_value=None,
                    ):
         """Add slider widget to alert."""
 
-        self.popup_button = AppKit.NSSlider.alloc().initWithFrame_(((0, 0), (slider_width, slider_height)))
-        if slider_color is not None:
-            bg = eval('AppKit.NSColor.%sColor()' % slider_color)
+        self.popup_button = AppKit.NSSlider.alloc().initWithFrame_(((0, 0), (width, height)))
+        if color is not None:
+            bg = eval('AppKit.NSColor.%sColor()' % color.lower())
             self.popup_button.setBackgroundColor_(bg)
         if slider_min_value is not None:
             self.popup_button.setMinValue_(slider_min_value)
@@ -103,62 +85,68 @@ class Alert:
             self.popup_button.setMaxValue_(slider_max_value)
         if slider_default_value is not None:
             self.popup_button.setValue_(slider_default_value)
-        if slider_tracker_color is not None:
-            color_trk = eval('AppKit.NSColor.%sColor()' % slider_tracker_color)
+        if tracker_color is not None:
+            color_trk = eval('AppKit.NSColor.%sColor()' % tracker_color.lower())
 
             self.popup_button.setTrackFillColor_(color_trk)
 
         self.alert.setAccessoryView_(self.popup_button)
 
-    def add_popup_button(self, popup_button_options: [str, list, tuple],
-                         popup_button_width, popup_button_height,
-                         popup_button_color, popup_button_controller_size: int,
-                         popup_button_border, popup_button_opacity: bool):
+    def add_popup_button(self, options: [str, list, tuple],
+                         width, height,
+                         color, controller_size: int,
+                         border, opacity: bool):
         """Add popup button to alert."""
-        self.popup_button_options = popup_button_options
+        self.popup_button_options = options
         self.pop_button = AppKit.NSPopUpButton.alloc().initWithFrame_(((0, 0),
-                                                                       (popup_button_width,
-                                                                        popup_button_height)))
+                                                                       (width,
+                                                                        height)))
 
-        if isinstance(popup_button_options, str):
-            self.pop_button.addItemWithTitle_(popup_button_options)
+        if isinstance(options, str):
+            self.pop_button.addItemWithTitle_(options)
         else:
-            for params in popup_button_options:
+            for params in options:
                 self.pop_button.addItemWithTitle_(params)
-        if popup_button_color is not None:
-            text_color = eval('AppKit.NSColor.%sColor()' % popup_button_color)
+        if color is not None:
+            text_color = eval('AppKit.NSColor.%sColor()' % color.lower())
             self.pop_button.setBackgroundColor_(text_color)
-        if popup_button_border is not None:
-            self.pop_button.setCornerRadius_(popup_button_border)
-        if popup_button_controller_size is not None:
+        if border is not None:
+            self.pop_button.setCornerRadius_(border)
+        if controller_size is not None:
             self.pop_button.setFlipped_(0)
-            self.pop_button.setControlSize_(popup_button_controller_size)
-            self.pop_button.setOpaque_(90)
-        if popup_button_opacity is not None:
-            self.pop_button.setTransparent_(int(popup_button_opacity))
+            self.pop_button.setControlSize_(controller_size)
+        if opacity is not None:
+            self.pop_button.setTransparent_(int(opacity))
         self.alert.setAccessoryView_(self.pop_button)
 
-    def add_radio_button(self, radio_button_width, radio_button_height,
-                         radio_button_color, radio_button_border,
-                         radio_button_font,  radio_button_font_size, radio_button_title,
-                         radio_button_state: [0, 1], radio_button_text_rotation: int):
-        self.radio = AppKit.NSButton.alloc().initWithFrame_(((0, 0), (radio_button_width, radio_button_height)))
-        self.radio.setButtonType_(AppKit.NSSwitchButton)
-        if radio_button_text_rotation is not None:
-            self.radio.setBoundsRotation_(radio_button_text_rotation)
-        if radio_button_state is not None:
-            self.radio.setState_(radio_button_state)
-        if radio_button_color is not None:
-            bg = eval('AppKit.NSColor.%sColor()' % radio_button_color)
-            self.radio.setBackgroundColor_(bg)
-        if radio_button_title is not None:
-            self.radio.setTitle_(radio_button_title)
-        if radio_button_border is not None:
-            self.radio.setCornerRadius_(radio_button_border)
-        if radio_button_font is not None and radio_button_font_size is not None:
-            self.radio.setFont_(AppKit.NSFont.fontWithName_size_(radio_button_font, radio_button_font_size))
+    def add_radio_button(self, options, width, height,
+                         color, border,
+                         font,  font_size, title,
+                         button_state: [0, 1]):
 
-        self.alert.setAccessoryView_(self.radio)  #
+        self.radio = AppKit.NSButton.alloc().initWithFrame_(((0, 0), (width, height)))
+        self.radio.setButtonType_(AppKit.NSSwitchButton)
+
+        if isinstance(options, str):
+            self.radio.setAccessibilityColumnTitles_(options)
+        else:
+            for params in options:
+                self.radio.setAccessibilityColumnTitles_(params)
+        if button_state is not None:
+            self.radio.setState_(button_state)
+        if color is not None:
+            bg = eval('AppKit.NSColor.%sColor()' % color.lower())
+            self.radio.setBackgroundColor_(bg)
+        if title is not None:
+            self.radio.setTitle_(title)
+        if border is not None:
+            self.radio.setCornerRadius_(border)
+        if font is not None and font_size is not None:
+            if font in self.list_available_fonts():
+                self.text_field.setFont_(AppKit.NSFont.fontWithName_size_(font, font_size))
+            raise NameError('Font not supported on device or not found')
+
+        self.alert.setAccessoryView_(self.radio)
 
     @property
     def entry_text(self):
@@ -189,14 +177,10 @@ class Alert:
     @property
     def radio_button_pressed(self):
         try:
-            return bool(int(self.radio.stringValue()))
+            return self.radio.stringValue()
         except AttributeError:
             raise AttributeError(f'Make object instance {self.add_radio_button.__name__} for get button state.')
 
-    def send(self):
-        """Necessary to call."""
-        self.runner = self.alert.runModal()
-        return self 
 
     @property
     def pressed_button(self):
@@ -205,9 +189,7 @@ class Alert:
         except KeyError:  # If in alert only one button
             return self.buttons
 
-
-
-
-
+    def send(self):
+        self.runner = self.alert.runModal()
 
 
